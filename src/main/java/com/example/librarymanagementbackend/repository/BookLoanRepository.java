9b05890c1371640df073600e3756020a46d88a7b
thanh2k4 <nvthanh12102004@gmail.com>
2025-05-04 22:19:08 +0700

    fix sql query

--- src/main/java/com/example/librarymanagementbackend/repository/BookLoanRepository.java
+++ src/main/java/com/example/librarymanagementbackend/repository/BookLoanRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface BookLoanRepository extends JpaRepository<BookLoan, Long> {
@@ -22,9 +21,10 @@ public interface BookLoanRepository extends JpaRepository<BookLoan, Long> {
                         @Param("status") BookLoanStatus status);
 
         @Query("SELECT COUNT(bl) FROM BookLoan bl WHERE (:userId IS NULL OR bl.user.id = :userId) " +
-                        "AND (:bookTitle IS NULL OR bl.bookCopy.book.title LIKE %:bookTitle%) " +
+                        "AND (:bookTitle IS NULL OR bl.bookCopy.book.title LIKE CONCAT('%', :bookTitle, '%')) " +
                         "AND (:status IS NULL OR bl.status = :status)")
         long countByFilters(@Param("userId") Long userId,
                         @Param("bookTitle") String bookTitle,
                         @Param("status") BookLoanStatus status);
+
 }
